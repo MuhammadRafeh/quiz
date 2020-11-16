540d2fa94239028a57fee9e25c79bf940a5a7ec6
Muhammad Rafeh <muhammadrafehatique@gmail.com>
2020-11-16 21:24:36 +0300

    Setting Navigation

--- App.js
+++ App.js
@@ -1,3 +1,20 @@
+
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+
+import QuizIndex from './screens/QuizIndex';
 import Quiz from './screens/Quiz';
 
-export default Quiz;
+const MainStack = createStackNavigator({
+  QuizIndex: {
+    screen: QuizIndex,
+    navigationOptions: {
+      headerTitle: 'Quizzes',
+    },
+  },
+  Quiz: {
+    screen: Quiz,
+  },
+});
+
+export default createAppContainer(MainStack);
